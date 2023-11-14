d69b7b28655338f1e28aef1d190731b99a47ee9a
Prathmesh Patil <129748752+Drago29599@users.noreply.github.com>
2023-11-14 19:29:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+function functionName() {
+    alert("functionName");
+}
+
+
+
+function AccioJob() {
+    alert("AccioJob()");
+}
+
+// Example of calling the function
+functionName();
+AccioJob()
